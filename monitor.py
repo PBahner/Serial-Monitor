c177a2506dad01521c93dae2724aa0c9f3002c3b
PBahner <kl.timo@outlook.de>
2021-02-08 21:07:07 +0100

    fixed #3 you can use ist over ssh now, tested on Linux/Windows

--- monitor.py
+++ monitor.py
@@ -1,15 +1,51 @@
+import os
 import serial
-import keyboard
+from getch import _Getch
+import threading
+import time
+
+
+class ListenOnSerialPort(threading.Thread):
+    def __init__(self, keyboard_thread):
+        threading.Thread.__init__(self)
+        self.keyboard_thread = keyboard_thread
+
+    def run(self):
+        while True:
+            if not self.keyboard_thread.input_active:
+                try:
+                    ser_in = ser.readline().decode('utf-8').strip('\n')
+                    print(ser_in)
+                except UnicodeDecodeError:
+                    print("Serial Error")
+
+
+class ListenOnKeyboard(threading.Thread):
+    input_active = False
+
+    def run(self):
+        while True:
+            cmd_in = getch()
+            print(cmd_in)
+            if cmd_in == "i":  # if key 'i' is pressed
+                self.input_active = True
+                time.sleep(0.1)
+                string = input(">> ")
+                ser.write(string.encode())
+                self.input_active = False
+            elif cmd_in == "q":
+                os._exit(1)
+
 
 print("Press 'i' for sending data")
+print("Press 'q' to quit")
 port = input("port: ")
 baud = 9600
 
+getch = _Getch()
 ser = serial.Serial(port, baud)
 
-while True:
-    ch = ser.readline().decode('utf-8').strip()
-    print(ch)
-    if keyboard.is_pressed('i'):  # if key 'i' is pressed
-        string = input(">> ")
-        ser.write(string.encode())
+kb_listen = ListenOnKeyboard()
+sp_listen = ListenOnSerialPort(kb_listen)
+kb_listen.start()
+sp_listen.start()
